e6d8189ed98371db3b1c8f8b816022be61e97161
Sagar Seth <lightningsagar0@gmail.com>
2025-06-18 14:31:56 +0530

    remove the protect route

--- route/contest.route.js
+++ route/contest.route.js
@@ -10,7 +10,7 @@ import { protectRoute } from "../Middleware/protectRoute.js";
 const route = express.Router();
 
 route.post("/create", protectRoute, createcontest);
-route.get("/", protectRoute, allContest);
+route.get("/", allContest);
 route.post("/fetch-multiple", protectRoute, fetch_multiple);
 route.post("/start", protectRoute, startContest);
 route.get("/rankings/:cid", protectRoute, rankcontest);
