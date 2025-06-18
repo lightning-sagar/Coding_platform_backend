672eeb7ccd7edde63a01fcd7da6db12f59c4509f
Sagar Seth <lightningsagar0@gmail.com>
2025-06-18 14:26:22 +0530

    added the cors

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ import ContestRoute from "./route/contest.route.js"
 import SubmitRoute from  "./route/submit.route.js"
 
 const app = express();
-app.use(cors("*"))
+app.use(cors({
+  origin: ["https://coding-platform-seven.vercel.app","https://coding-platform-seven.vercel.app/"],
+  credentials: true, 
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
